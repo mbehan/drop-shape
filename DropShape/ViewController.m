db420577a51f99d4fa82750a70b341934cb1e9f5
Michael Behan <michael@marinosoftware.com>
2014-02-06 22:05:24 +0000

    creating view from drawn path and adding gravity behaviour

--- DropShape/ViewController.m
+++ DropShape/ViewController.m
@@ -9,14 +9,27 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIDynamicAnimator *animator;
+    UIGravityBehavior *gravity;
+}
 @end
 
 @implementation ViewController
 
 -(void)drawingViewCreatedPath:(UIBezierPath *)path
 {
-    NSLog(@"HAVE SHAPE");
+    UIView *shapeView = [[UIView alloc] initWithFrame:self.view.bounds];
+    
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = path.CGPath;
+    shapeLayer.fillColor = [UIColor blackColor].CGColor;
+    
+    [shapeView.layer addSublayer:shapeLayer];
+    
+    [self.view addSubview:shapeView];
+    
+    [gravity addItem:shapeView];
 }
 
 - (void)viewDidLoad
@@ -26,6 +39,11 @@
     // Do any additional setup after loading the view, typically from a nib.
     SimplePathDrawingView *drawingView = (SimplePathDrawingView *)self.view;
     drawingView.delegate = self;
+    
+    animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    gravity = [[UIGravityBehavior alloc] init];
+    [animator addBehavior:gravity];
+    
 }
 
 - (void)didReceiveMemoryWarning
