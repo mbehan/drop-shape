d4a54cdda702a205227496ed3549edc493f248f9
Michael Behan <michael@marinosoftware.com>
2014-02-10 00:02:39 +0000

    renamed files

--- DropShape/UIBezierPath-Thinning.h
+++
@@ -1,9 +0,0 @@
-/*
- Erica Sadun, http://ericasadun.com
- iPhone Developer's Cookbook, 6.x Edition
- BSD License, Use at your own risk
- */
-
-#import <Foundation/Foundation.h>
-
-UIBezierPath *thinPath(UIBezierPath *path, CGFloat tolerance);
